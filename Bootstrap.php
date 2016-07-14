a80264f7a89d18b36601dfba30620458c44f010e
Sven Münnich <sven.muennich@viison.com>
2016-07-14 10:56:36 +0200

    Prepare release of v1.0.9

--- Bootstrap.php
+++ Bootstrap.php
@@ -20,7 +20,7 @@ class Shopware_Plugins_Frontend_StripePayment_Bootstrap extends Shopware_Compone
 	 * @return The current version of this plugin.
 	 */
 	public function getVersion() {
-		return '1.0.8';
+		return '1.0.9';
 	}
 
 	/**
@@ -135,6 +135,8 @@ class Shopware_Plugins_Frontend_StripePayment_Bootstrap extends Shopware_Compone
 			case '1.0.7':
 				// Nothing to do
 			case '1.0.8':
+				// Nothing to do
+			case '1.0.9':
 				// Next release
 				break;
 			default:
